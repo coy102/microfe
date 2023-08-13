3a971cfac396b5e9adcba199d3687d7ab817c164
Fajar Septiawan <Fajar.Septiawan@xapiens.id>
2023-08-13 18:21:16 +0700

    Initial commit

---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MarketingApp from "./components/MarketinApp";
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+
+      <MarketingApp />
+    </div>
+  );
+};
